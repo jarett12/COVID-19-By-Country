226fd38c8b778c4466f46ba81c63d63021cc2f1a
Jarett Antolin <jarett@Jaretts-Air.fios-router.home>
2020-09-06 05:23:33 -0400

    Updated Settings

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://covid19-by-country.herokuapp.com/']
+ALLOWED_HOSTS = ['covid19-by-country.herokuapp.com']
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 # Application definition
