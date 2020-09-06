95c9389c7475d4680dc72a9396e9f1709929c7f4
Jarett Antolin <jarett@Jaretts-Air.fios-router.home>
2020-09-06 05:20:38 -0400

    Updated Settings

--- covid19/models.py
+++ covid19/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class COVID(models.Model):
-    globalStats= models.TextField()
-    countries = models.TextField()
-
-    def __str__(self):
-        return self.globalStats
