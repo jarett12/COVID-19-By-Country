c5105a6c6fcac1f8f446fedc3780ddb6b7f0e98c
Jarett Antolin <jarett@Jaretts-Air.fios-router.home>
2020-09-06 04:34:35 -0400

    Updated STATIC_ROOT

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -115,5 +115,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles_')
 STATIC_URL = '/static/'
